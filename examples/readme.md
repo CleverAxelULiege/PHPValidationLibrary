af362cb14dd0746a662dc0bb7f0557b85002d0ad
Axel <clever.axel95@gmail.com>
2023-11-15 08:00:35 +0100

    renamed showroom to examples

---
+++ examples/readme.md
@@ -0,0 +1,8 @@
+Pour que la validation marche, il lui faut deux ARRAYS associatifs.
+Un qui lui spécifie les règles à appliquer dans un tableau associatif et un autre avec les données à analyser.
+
+Les tableaux sont mis en communs via une clef commune.
+
+Le tableau de règles peuvent être NULL ou REQUIS par défaut ils sont à NULL
+
+Voir validation1.php, validation2.php ou validation3.php pour voir différents scénarios d'applications
---
+++ examples/validation3.php
@@ -0,0 +1,40 @@
+<?php
+
+require(__DIR__ . "/../vendor/autoload.php");
+
+use App\Validation\Rules\MustBeAfterOrEqualsDateRule;
+use App\Validation\Rules\MustBeAfterOrEqualsTimeRule;
+use App\Validation\Rules\MustBeBeforeOrEqualsDateRule;
+use App\Validation\Rules\MustBeBeforeOrEqualsTimeRule;
+use App\Validation\Rules\NullableRule;
+use App\Validation\Validator;
+
+$data = [
+    "start_date" => "2023/10/31",
+    "end_date" => "2023/12/31",
+    "start_time" => "08:00",
+    "end_time" => "19:00",
+];
+
+$validationRules = [
+    "start_date" => [
+        new NullableRule(),
+        new MustBeBeforeOrEqualsDateRule("end_date", true),
+    ],
+    "end_date" => [
+        new NullableRule(),
+        new MustBeAfterOrEqualsDateRule("start_date", true),
+    ],
+    "start_time" => [
+        new NullableRule(),
+        new MustBeAfterOrEqualsTimeRule("08:00"),
+        new MustBeBeforeOrEqualsTimeRule("end_time", true),
+    ],
+    "end_time" => [
+        new NullableRule(),
+        new MustBeBeforeOrEqualsTimeRule("20:00"),
+        new MustBeAfterOrEqualsTimeRule("start_time", true),
+    ]
+];
+
+Validator::rawDisplay(new Validator($validationRules, $data));
